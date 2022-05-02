7566f8edd203aab6733cf9274f263c7d1cad1440
arturpmorais <arturpmrs@gmail.com>
2022-05-01 21:57:03 -0300

    Creating the type "Car", moving the mock car array to the ngOnInit() method to support future implementations like fetching data from APIs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { VERSION } from '@angular/compiler';
-import { of } from 'rxjs'
+import { Observable, of } from 'rxjs'
+
+type Car = {
+  manufacturer: string;
+  model: string;
+  year: number;
+  img: string;
+  available: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,41 +16,43 @@ import { of } from 'rxjs'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  cars: Observable<Car[]>;
+
+  constructor() {
+    this.cars = of<Car[]>([])
+  };
+
+  ngOnInit(): void {
+    this.cars = of<Car[]>([
+      {
+        manufacturer: 'Honda',
+        model: 'Civic',
+        year: 2009,
+        img: 'assets/civic-2009.jpg',
+        available: true
+      },
+      {
+        manufacturer: 'Peugeot',
+        model: '206',
+        year: 2004,
+        img: 'assets/206-2004.jpg',
+        available: false
+      },
+      {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        year: 2022,
+        img: 'assets/golf-2022.jpg',
+        available: false
+      }
+    ])
+  };
 
   clicked = false;
   title = `Running on Angular ${VERSION.full}!`;
 
-  cars = of([
-    {
-      manufacturer: 'Honda',
-      model: 'Civic',
-      year: 2009,
-      img: 'assets/civic-2009.jpg',
-      available: true
-    },
-    {
-      manufacturer: 'Peugeot',
-      model: '206',
-      year: 2004,
-      img: 'assets/206-2004.jpg',
-      available: false
-    },
-    {
-      manufacturer: 'Volkswagen',
-      model: 'Golf',
-      year: 2022,
-      img: 'assets/golf-2022.jpg',
-      available: false
-    }
-  ])
-
   handleClick() {
     this.clicked = true;
   }
-
-  constructor() { };
-
-  ngOnInit(): void {
-  };
 
 } 
